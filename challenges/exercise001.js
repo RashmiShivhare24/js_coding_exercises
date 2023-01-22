36d15ab2e444a999a120527723fb90417fe58a52
Rashmi Shivhare <wagh.rashmi@gmail.com>
2023-01-22 20:26:35 +0000

    Implemented coding best practices based on Neil's checklist & refactored the code

--- challenges/exercise001.js
+++ challenges/exercise001.js
@@ -2,7 +2,7 @@
 
 export function capitalize(word) {
   if (word === undefined) throw new Error("word is required");
-  return word.charAt(0).toUpperCase() + word.substring(1,word.length);
+  return word.charAt(0).toUpperCase() + word.substring(1, word.length);
 }
 
 export function generateInitials(firstName, lastName) {
@@ -14,42 +14,42 @@ export function generateInitials(firstName, lastName) {
 export function addVAT(originalPrice, vatRate) {
   if (originalPrice === undefined) throw new Error("originalPrice is requied");
   if (vatRate === undefined) throw new Error("vatRate is required");
-  let VAT = (originalPrice/100) * vatRate;
-  VAT = Number.isInteger(VAT) ? Number(VAT + originalPrice).toFixed(0): Number(VAT + originalPrice).toFixed(2);
-  return Number(VAT) ;
+  let VAT = (originalPrice / 100) * vatRate;
+  VAT = Number.isInteger(VAT) ? Number(VAT + originalPrice).toFixed(0) : Number(VAT + originalPrice).toFixed(2);
+  return Number(VAT);
 }
 
 export function getSalePrice(originalPrice, reduction) {
   if (originalPrice === undefined) throw new Error("originalPrice is required");
   if (reduction === undefined) throw new Error("reduction is required");
-  let salePrice = (originalPrice/100)*reduction;
-  salePrice = Number.isInteger(salePrice)? Number(originalPrice - salePrice).toFixed(0): Number(originalPrice-salePrice).toFixed(2);
+  let salePrice = (originalPrice / 100) * reduction;
+  salePrice = Number.isInteger(salePrice) ? Number(originalPrice - salePrice).toFixed(0) : Number(originalPrice - salePrice).toFixed(2);
   return Number(salePrice);
 }
 
 export function getMiddleCharacter(str) {
   if (str === undefined) throw new Error("str is required");
-    var position;
-    var length;
-      
-      if(str.length % 2 == 1) {
-        position = str.length / 2;
-        length = 1;
-      } else {
-        position = str.length / 2 - 1;
-        length = 2;
-      }
-return str.substring(position, position + length);
+  let position;
+  let length;
+
+  if (str.length % 2 === 1) {
+    position = str.length / 2;
+    length = 1;
+  } else {
+    position = str.length / 2 - 1;
+    length = 2;
+  }
+  return str.substring(position, position + length);
 }
 
 export function reverseWord(word) {
   if (word === undefined) throw new Error("word is required");
   /* Splitting word into array, reversing the array & then joining all elements of array into string */
-  var splitWord = word.split("");
-  var reverseArray = splitWord.reverse();
-  var joinArray = reverseArray.join("");
+  let splitWord = word.split("");
+  let reverseArray = splitWord.reverse();
+  let joinArray = reverseArray.join("");
   return joinArray;
-// Can be refactored to 1 LOC: word.split().reverse().join("");
+  // Can be refactored to 1 LOC: word.split().reverse().join("");
 }
 
 export function reverseAllWords(words) {
@@ -65,24 +65,24 @@ export function countLinuxUsers(users) {
 
 export function getMeanScore(scores) {
   if (scores === undefined) throw new Error("scores is required");
-      var average = "";
-      var sum = 0;
-        for (var number of scores) {
-        sum += number;
-        }
-      average = Number.isInteger?(sum / scores.length).toFixed(2): (sum / scores.length).toFixed(0);
+  let average = "";
+  let sum = 0;
+  for (let number of scores) {
+    sum += number;
+  }
+  average = Number.isInteger ? (sum / scores.length).toFixed(2) : (sum / scores.length).toFixed(0);
   return Number(average);
 }
 
 export function simpleFizzBuzz(n) {
   if (n === undefined) throw new Error("n is required");
 
-    if (n % 3 == 0 && n % 5 == 0)
-        return("fizzbuzz");
-    else if (n % 5 == 0)
-        return("buzz");
-    else if (n % 3 === 0)
-        return("fizz");
-    else
-        return(n);
+  if (n % 3 === 0 && n % 5 === 0)
+    return ("fizzbuzz");
+  else if (n % 5 === 0)
+    return ("buzz");
+  else if (n % 3 === 0)
+    return ("fizz");
+  else
+    return (n);
 }
