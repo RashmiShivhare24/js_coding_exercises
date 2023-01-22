d59dce5a359ef2c5f6f5d1fc3341298f88b32b98
Rashmi Shivhare <wagh.rashmi@gmail.com>
2023-01-22 01:12:33 +0000

    Implemented first exercise -capitalize

--- challenges/exercise001.js
+++ challenges/exercise001.js
@@ -2,7 +2,9 @@
 
 export function capitalize(word) {
   if (word === undefined) throw new Error("word is required");
-  // Add your code here!
+  console.log(word.charAt(0).toUpperCase() + word.substring(1,word.length));
+  return word.charAt(0).toUpperCase() + word.substring(1,word.length);
+  
 }
 
 export function generateInitials(firstName, lastName) {
