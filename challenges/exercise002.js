e6cd97268617fd9603bc6a77bccfe2026c19e6ea
Rashmi Shivhare <wagh.rashmi@gmail.com>
2023-01-23 18:56:37 +0000

    Implemented all functions in Exercise 002

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -1,24 +1,32 @@
 export function getFillings(sandwich) {
   if (sandwich === undefined) throw new Error("ingredients is required");
-  // Your code here!
+    return sandwich.fillings;
 }
 
 export function isFromManchester(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
+   return person.city === "Manchester" ? true: false;
 }
 
 export function getBusNumbers(people) {
   if (people === undefined) throw new Error("people is required");
-  // Your code here!
+    let numberOfBuses = 0;
+    return numberOfBuses = people % 40 === 0 ? people/40 : Math.trunc(people/40)+1;
 }
 
 export function countSheep(arr) {
   if (arr === undefined) throw new Error("arr is required");
-  // Your code here!
+  let sheepCount = 0;
+
+    for(let animal of arr)
+    {
+      animal === "sheep" ? sheepCount +=1 : sheepCount +=0;
+    }
+
+  return sheepCount;
 }
 
 export function hasMPostCode(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
+   return person.address.postCode.charAt(0) === "M" && person.address.city === "Manchester" ;
 }
